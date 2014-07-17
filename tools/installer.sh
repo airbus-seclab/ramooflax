ff1d90fbce7a60f6a39304809d20709e381eef79
stephane duverger <stephane.duverger@gmail.com>
2014-07-17 14:31:28 +0200

    clean exit code on installer tool

--- tools/installer.sh
+++ tools/installer.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 VMNAME="Debian 7 64-bit"
+
 VMPATH="/media/psf/Home/Vmware/${VMNAME}.vmwarevm"
+VMDISK="${VMPATH}/${VMNAME}.vmdk"
+VMMNT="${VMPATH}/mnt"
 
-vmware-mount "${VMPATH}/${VMNAME}.vmdk" 1 "${VMPATH}/mnt"
-cp $1 "${VMPATH}/mnt/boot/$(basename $1)"
+vmware-mount ${VMDISK} 1 ${VMMNT} || exit 1
+cp $1 "${VMMNT}/boot/$(basename $1)" || exit 1
 vmware-mount -X 2>/dev/null
-
 exit 0
